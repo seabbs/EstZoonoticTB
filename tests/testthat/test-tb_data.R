80de7a7d2d0f7bec69fff094bdb5f158e1d2d5fe
seabbs <s.e.abbott12@gmail.conm>
2019-09-19 11:58:22 +0000

    added testing for TB data

--- tests/testthat/test-tb_data.R
+++ tests/testthat/test-tb_data.R
@@ -1,4 +1,17 @@
 context("tb_data")
 
 
-## This is a dummy test file.
+test_that("TB data can be successfully extracted", {
+  skip_on_cran()
+  expect_true(!is.null(tb_data())) 
+})
+
+test_that("TB data has the expected number of columns",{
+  skip_on_cran()
+  expect_equal(ncol(tb_data()), 11)
+})
+
+test_that("TB data has at least 1 row",{
+  skip_on_cran()
+  expect_true(nrow(tb_data()) > 0)
+})
